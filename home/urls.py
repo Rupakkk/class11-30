9a1cad88c57f30eeb25b6b1fab01ca43b21a6322
Rupakkk <rupak.fcc@gmail.com>
2021-03-19 15:18:43 +0545

    4th commit

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home,name="home")
+    path('',home,name="home"),
+    path('about',about,name="about"),
+    path('blog-home',bloghome,name="blog-home"),
+    path('blog-single',blogsingle,name="blog-single"),
+    path('contact',contact,name="contact"),
+    path('elements',elements,name="elements"),
+    path('portfolio',portfolio,name="portfolio"),
+    path('price',price,name="price"),
+    path('services',services,name="services"),
 ]
 
 
